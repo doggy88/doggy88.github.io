19d1dcac440dbb8f4a0131cd16d63841b70713ae
doggy88 <lego.radeon@gmail.com>
2021-12-05 20:43:06 +0100

    ss
    ss

---
+++ js/script.js
@@ -0,0 +1,377 @@
+let breakfastz = [
+    "Áfonyás grilled cheese szendvics",
+    "Alap tojáskrém",
+    "Angol reggeli",
+    "Babkrémes-sonkás-sajtos gofri",
+    "Babos-kolbász pirítós",
+    "Baconos röszti rántottával",
+    "Bundáskenyér",
+    "Bundáskenyér szendvics",
+    "Buggyantott tojás pirítóssal",
+    "Csirkés szendvics",
+    "egg-mayo sandwich",
+    "felvágottas kenyér",
+    "Fokhagymás vajas pirítós",
+    "Főtt Virsli",
+    "Gombás melegszendvicskrém",
+    "Grilled-Cheese Sandwich",
+    "Gyors amerikai palacsinta",
+    "Hagymás rántotta",
+    "Hagymás-bab",
+    "Hal-konzerv",
+    "Japán Palacsinta",
+    "Konzerv melegszendvicskrém",
+    "Lekváros-kenyér",
+    "Lilahagymás tojáskrém",
+    "löncshús",
+    "Melegszendvics",
+    "Muffuletta",
+    "Omlett",
+    "Parasztreggeli",
+    "Pesztós-Pulykás grillszendvics",
+    "Rántotta",
+    "Sajtos Omlett",
+    "Sonkás-sajtos bagett",
+    "Sonkás-Tojás",
+    "Spanish omlette sandwich style",
+    "sült szalonna",
+    "Sült Virsli",
+    "Szalonnás hagymás rántotta",
+    "Tejbe-gríz",
+    "Tejbe-rizs",
+    "Tepertőkrém",
+    "tonhal-sali",
+    "Tuna & Quinoa Patties",
+    "Tuna-Fasírt",
+    "Tuna-Sandwich",
+    "Tükörtojás",
+    "Vajas-mézes kenyér",
+    "Zabpehely (corn flakes)",
+    "zsíros kenyér",
+    "zsíros pirítós hagymával",
+];
+
+
+
+let lunch = ["Amerikai szaftos tarja krumplival",
+"Apple pork casserole",
+"Bazsalikomos Rakott tészta",
+"Black Bean sauce green pepper Beef",
+"Bolonyai",
+"Brassói anya féle",
+"Brassói Ica néni féle",
+"Breakfast casserole",
+"Burgundi Marharagu",
+"Carbonara spagetti",
+"Cézár Salis csirke",
+"Cheezy Broccoli Pasta",
+"Chicken curry",
+"Chicken Korma",
+"Chicken mushroom casserole",
+"Chicken Mushroom cobbler",
+"Chicken Tikka Masala",
+"Chimichanga",
+"Cigány pecsenye",
+"Cordon bleu",
+"Cottage Pie",
+"Csilisbab",
+"Csípős csirkeszárnyak",
+"Csirkepörkölt",
+"Csirkés Burrito",
+"Csirkés rétes",
+"Csirkés Rizottó gombával",
+"Csirkés-tejszínes penne",
+"Csülök pékné módra",
+"Darálthússal töltött paprika",
+"Derelye",
+"Disznópörkölt",
+"Édes-savanyú csirke",
+"Édesburgonyás gnocchi",
+"Egyben sült karaj",
+"Enchilladas",
+"Fajitas",
+"Fasírt",
+"Fish n Chips",
+"Fokhagymás Parmezános tészta",
+"Fokhagymás sertés csíkok csípősen zöldségekkel",
+"Főtt sonka",
+"Gnocchi with Tomato Sauce",
+"Gombapörkölt",
+"Gombás-Sajtos tepsis hús",
+"Gombás-Tejfölös sertés szelet galuskával",
+"Gombóta",
+"Gong-Bao csirke (mogyorós)",
+"Grízes tészta",
+"Gyros",
+"Gyros Pitában",
+"Hagymakrémes cigánypecsenye",
+"Hagymás marha rostélyos",
+"Hoszin szószos kínai húsgolyók",
+"Hot Pot",
+"Hot-Dog",
+"Húsos palacsinta",
+"Illatos omlós csirkemell",
+"KFC csirke",
+"Kínai sült tészta csirkés",
+"Kung pao chicken",
+"Lazannya",
+"Lemon garlic sticky chicken",
+"Lepcsánka",
+"Mac Cheese",
+"Marha steak",
+"Marhahúsos burrito",
+"Marhapörkölt",
+"Meggyszósz Sült hússal",
+"Mexikói rizses darálthús egytálétel",
+"Mézes-Mustáros csirke",
+"Milánói makaróni",
+"Mogyorós-csípős csirke",
+"Mushroom chicken pie",
+"Nachos",
+"Nudli",
+"Olaszos-tejszínes gombaszósz spagettivel",
+"Pad Thai",
+"Paprikás krumpli",
+"Paradicsom szószos bazsalikomos tészta békönnel",
+"Parasztreggeli",
+"Pesztós csirke rolád",
+"Piri-Piri sült csirke",
+"Pizza",
+"Pulled Pork",
+"Quesadilla",
+"Rakott krumpli",
+"Rántott hús",
+"Rántott parizer",
+"Rántott sajt",
+"Rizseshús",
+"Sajtos-csirkés rakott karfiol",
+"Sausage in onion gravy with mash",
+"Shepeard pie",
+"Shish Kebab",
+"Slambuc",
+"Sonkás-hagymás-tojásos rakott tészta",
+"Spaghetti Tuna & Tomato Sauce",
+"Stefánia szelet",
+"Sticky Honey Pork",
+"Sunday Roast",
+"Sült csirke comb",
+"Sült csirke egészbe",
+"Sült kacsa dinsztelt káposztával",
+"Sült oldalas",
+"Szecsuáni Csirke",
+"Szecsuáni Marha",
+"Székelykáposzta",
+"Szezámos kínai csirke",
+"Szilvásgombóc",
+"Szűzpecsenye lecsós burgonyával",
+"Tarhonyás sertéspörkölt",
+"tatár beefsteak",
+"Tejfölben pácolt csirkemell tepsis reszelt krumplival",
+"Tepsis krumpli",
+"Teriyaki csirke",
+"Tésztasaláta",
+"Tonhalas tészta",
+"Töltött káposzta",
+"Töltött sült-csirkecomb",
+"Túrógombóc",
+"Túrós csusza",
+"Vega lazannya",
+"Vegetáriánus curry",
+"Zöld borsó főzelék",
+
+];
+
+let leves = ["Babgulyás",
+"Bableves",
+"Brokkolikrémleves",
+"Carrot & Coriander",
+"Édesburgonya krémleves",
+"Fokhagymakrémleves",
+"Gombakrémleves",
+"Grízgombócleves",
+"Gulyásleves",
+"Halászlé",
+"Húsgombóc leves",
+"Húsleves",
+"Jókai bableves",
+"Karfiolleves",
+"Kelbimbó leves",
+"Kertészleves",
+"Kínai leves",
+"Köményleves",
+"Krumplileves",
+"Lebbencsleves",
+"Májgombócleves",
+"Minestoneleves",
+"Paradicsomleves",
+"Spárgakrémleves",
+"Tarhonyaleves",
+"Tárkonyosraguleves",
+"Zellerkrémleves",
+"Zöldborsóleves",
+"Zöldségleves",
+];
+
+let desszert = ["3bit szelet",
+"Amerikai Palacsinta",
+"Bagolyszem",
+"Beigli",
+"Bukta",
+"Carrot cake",
+"Cheese cake",
+"Chocolate brownies",
+"Chocolate fudge cake",
+"Citromos tart",
+"Csoki bomba",
+"Csoki torta",
+"Csokis vaniliás csigák",
+"Csokoládés pattogós sütemény",
+"Csőröge",
+"Darálóskeksz",
+"Egytojásos torta",
+"Fánk",
+"Fonott kalács",
+"Goffri",
+"Gyümölcs torta",
+"Hókifli",
+"Jugó kifli",
+"Kakaós csiga",
+"Karamellás short-bread",
+"Kavart csokis túrós",
+"Kavart diós",
+"Kenyérlángos",
+"Képviselő fánk",
+"Kinder buenó szelet",
+"Kókusz golyó",
+"Kókusz kocka",
+"Krémes",
+"Krumplis pogácsa",
+"Kürtös kalács",
+"Lángos",
+"Linzer",
+"Madártej szelet",
+"Mikrós süti",
+"Muffin",
+"Nap és éj",
+"Napraforgó",
+"Őzgerinc",
+"Palacsinta",
+"Paplan sütemény",
+"Piskóta forró csokival",
+"Piskóta tekercs",
+"Pizzás-szalámis mnicsiga",
+"Pogácsa",
+"Pudingos szelet",
+"Ropogós sajtosrúd",
+"Sajtos kifli",
+"Sajtos rolló",
+"Sajtos rúd",
+"Sajtos sós perec",
+"Sajtos tallér",
+"Somlói galuska",
+"Sport szelet",
+"Tiramisu",
+"Túrós-Sajtos",
+"Vaniliás karika",
+"Vendégváró süti",
+"Zserbó",
+];
+
+
+
+
+let chosenList = [];
+
+let nameGen = document.getElementById("name");
+let nameGenBtn = document.getElementById("nameGenBtn")
+let list1Btn = document.getElementById("list1");
+let list2Btn = document.getElementById("list2");
+let list3Btn = document.getElementById("list3");
+let list4Btn = document.getElementById("list4");
+
+
+
+
+list1Btn.addEventListener("click", BreakfastBtnChecked);
+list2Btn.addEventListener("click", LunchBtnChecked);
+list3Btn.addEventListener("click", LevesBtnChecked);
+list4Btn.addEventListener("click", DessertBtnChecked);
+
+
+
+
+nameGenBtn.addEventListener("click", NameGenerator);
+
+function NameGenerator(){
+    
+    if(list1Btn.checked){
+        let randomNumber = Math.floor(Math.random() * breakfastz.length);
+       nameGen.innerText = breakfastz[randomNumber]; }
+    
+    if(list2Btn.checked){
+        let randomNumber = Math.floor(Math.random() * lunch.length)
+        nameGen.innerText = lunch[randomNumber]; }
+    
+        if(list3Btn.checked){
+        let randomNumber = Math.floor(Math.random() * leves.length)
+        nameGen.innerText = leves[randomNumber]; }
+        
+        if(list4Btn.checked){
+        let randomNumber = Math.floor(Math.random() * desszert.length)
+        nameGen.innerText = desszert[randomNumber]; }
+        
+
+
+
+
+}
+
+
+function chooseList(list){
+    chosenList = list
+}
+
+
+function BreakfastBtnChecked(){
+    if (list1Btn.checked){
+        list1Btn.checked = true;
+        list2Btn.checked = false;
+        list3Btn.checked = false;
+        list4Btn.checked = false;
+    }
+    chooseList(Breakfastz);
+}
+function LunchBtnChecked(){
+    if(list2Btn.checked){
+        list2Btn.checked = true;
+        list1Btn.checked = false;
+        list3Btn.checked = false;
+        list4Btn.checked = false;
+    }
+    chooseList(Lunch);
+}
+
+function LevesBtnChecked(){
+    if(list3Btn.checked){
+        list2Btn.checked = false;
+        list1Btn.checked = false;
+        list3Btn.checked = true;
+        list4Btn.checked = false;
+    }
+    chooseList(Leves);
+}
+
+function DessertBtnChecked(){
+    if(list4Btn.checked){
+        list2Btn.checked = false;
+        list1Btn.checked = false;
+        list3Btn.checked = false;
+        list4Btn.checked = true;
+        
+    }
+    chooseList(Leves);
+}
+
+
+    
+
